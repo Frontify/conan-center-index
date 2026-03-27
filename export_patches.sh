ef573160d48500edb98b71ce41dc76a02a1dbf2e
Nicolas Koller <48681214+kolibri91@users.noreply.github.com>
2026-03-27 09:09:46 +0100

    chore: export script

---
+++ export_patches.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+# shellcheck source=/dev/null
+
+root_dir="$(realpath "$(dirname "$0")")"
+
+if ! conan --version > /dev/null 2>&1
+then
+    if ! source "${root_dir}/.venv-conan/bin/activate"
+    then
+        python3 -m venv "${root_dir}/.venv-conan"
+        source "${root_dir}/.venv-conan/bin/activate"
+        python -m pip install "conan<4.0"
+    fi
+fi
+
+user="frontify"
+channel="stable"
+
+conan export "${root_dir}/recipes/cargo" --version 1.0 --user "${user}" --channel "${channel}"
+conan export "${root_dir}/recipes/librsvg/all" --version 2.58.5 --user "${user}" --channel "${channel}"
+conan export "${root_dir}/recipes/libvips/all" --version 8.15.2 --user "${user}" --channel "${channel}"
+conan export "${root_dir}/recipes/ffmpeg/all" --version 4.4.6 --user "${user}" --channel "${channel}"
